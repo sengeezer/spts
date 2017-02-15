e04c0cbba46dbc17e48156326cad32cfd0edfde9
Fabio Albertin <fabio.albertin@matchesfashion.com>
2017-02-15 18:35:49 +0000

    Finished part 1

--- tutorial.js
+++ tutorial.js
@@ -6,4 +6,45 @@ car.toString = function () {
     return _this.numberPlate + " (" + _this.numberOfWheels + " wheels)";
 };
 console.log(car.toString());
+// can be either type
+var eitherType = '5';
+// hence won't complain
+eitherType = parseInt(eitherType, 10);
+// type can be freed
+var whatever = {};
+// objects
+var myObj = { 0: 1, 1: 2 };
+// arrs
+var members = ['Steve', 'Geoffrey'];
+// generic
+var otherMembers = Array();
+// annotated function
+var getMaxVal = function (num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+};
+// also valid:
+var getOtherMaxVal = function (num1, num2, num3) {
+    return Math.max(num1, num2, num3);
+};
+getMaxVal(3, 4, 66);
+// optional and default value params
+var sum = function (a, b) {
+    return a + (b || 0);
+};
+var sums = function (a, b) {
+    if (b === void 0) { b = 0; }
+    return a + b;
+};
+// if no return, use void
+var log = function (s) {
+    console.log(s);
+};
+log('ess');
+var otherGetMax = function () {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    return Math.max.apply(Math, nums);
+};
 //# sourceMappingURL=tutorial.js.map
--- tutorial.ts
+++ tutorial.ts
@@ -24,3 +24,35 @@ let myObj: Object = { 0: 1, 1: 2};
 let members: string[] = ['Steve', 'Geoffrey'];
 // generic
 let otherMembers = Array<string>();
+
+// annotated function
+let getMaxVal = (num1: number, num2: number, num3: number): number => {
+    return Math.max(num1, num2, num3);
+};
+// also valid:
+let getOtherMaxVal: (a: number, b: number, c: number) => number = (num1, num2, num3) => {
+    return Math.max(num1, num2, num3);
+};
+
+getMaxVal(3, 4, 66);
+
+// optional and default value params
+let sum = (a: number, b?: number): number => {
+    return a + (b || 0);
+}
+
+let sums = (a: number, b: number = 0): number => {
+    return a +b;
+}
+
+// if no return, use void
+let log = (s: string): void => {
+    console.log(s);
+}
+
+log('ess');
+
+// Use any number of args
+let otherGetMax = (...nums: number[]): number => {
+    return Math.max(...nums);
+}
